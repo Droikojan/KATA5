30bcb465039ef05c381b1eca45fa12075d3eab68
usuario <usuario@10.22.143.35>
2015-11-11 10:06:55 +0000

    version 2

--- src/kata5/KATA5.java
+++ src/kata5/KATA5.java
@@ -1,5 +1,10 @@
 package kata5;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -8,18 +13,29 @@ import java.sql.Statement;
 
 public class KATA5 {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, FileNotFoundException, IOException {
+
         Class.forName("org.sqlite.JDBC");
         Connection conec = DriverManager.getConnection("jdbc:sqlite:DDBBKATA");
-        
+
         Statement state = conec.createStatement();
         ResultSet rs = state.executeQuery("SELECT * FROM PEOPLE");
-        
-        while (rs.next()){
+
+        while (rs.next()) {
             System.out.println("ID = " + rs.getInt("ID"));
             System.out.println("NAME = " + rs.getString("NAME"));
         }
-        
+
+
+        BufferedReader reader = new BufferedReader(new FileReader(new File("emailsfilev1.txt")));
+
+        String mail;
+        while ((mail = reader.readLine()) != null) {
+            state.executeUpdate("INSERT INTO MAIL (MAIL) VALUES ('" + mail + "')");
+        }
+
+        rs.close();
+        state.close();
+        conec.close();
     }
 }
